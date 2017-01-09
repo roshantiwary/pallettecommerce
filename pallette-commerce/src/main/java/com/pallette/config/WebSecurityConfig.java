de1c7b4ed308d4989035d685b00f978b1c8c11ab
Roshan <roshantiwary@gmail.com>
2017-01-09 17:24:46 -0500

    updated authentication logic

--- pallette-commerce/src/main/java/com/pallette/config/WebSecurityConfig.java
+++ pallette-commerce/src/main/java/com/pallette/config/WebSecurityConfig.java
@@ -2,16 +2,25 @@ package com.pallette.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.pallette.web.security.CustomAuthenticationProvider;
+import com.pallette.web.security.CustomCredentialsService;
 import com.pallette.web.security.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private CustomCredentialsService customCredentialsService;
+	
+	@Autowired
+	private CustomAuthenticationProvider customAuthenticationProvider;
 	
 	@Autowired
 	private LogoutSuccessHandler logoutSuccessHandler;
@@ -41,5 +50,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/webjars/**")
                 .antMatchers("/images/**").antMatchers("/css/**").antMatchers("/js/**");
     }
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(customAuthenticationProvider);
+		auth.userDetailsService(customCredentialsService);
+	}
 
 }
