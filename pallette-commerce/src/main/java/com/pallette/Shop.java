35239a118a01758a0a52c24c15f5afc762b8e8c4
Roshan <roshantiwary@gmail.com>
2017-02-14 18:40:22 +0530

    fixed oauth2 rest client implementation

--- pallette-commerce/src/main/java/com/pallette/Shop.java
+++ pallette-commerce/src/main/java/com/pallette/Shop.java
@@ -1,13 +1,30 @@
 package com.pallette;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenProvider;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableOAuth2Client
 public class Shop extends SpringBootServletInitializer{
 	    
 	@Override
@@ -20,4 +37,50 @@ public class Shop extends SpringBootServletInitializer{
 		SpringApplication.run(Shop.class, args);	
 	}
 	
+	@Value("${security.oauth2.client.access-token-uri}")
+	private String accessTokenUri;
+
+    @Value("${security.oauth2.client.grant-type}")
+    private String grantType;
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientID;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+	
+	/**
+     * The heart of our interaction with the resource; handles redirection for authentication, access tokens, etc.
+     * @param oauth2ClientContext
+     * @return
+     */
+    @Bean
+    public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {    	
+//    	ClientCredentialsAccessTokenProvider provider = new ClientCredentialsAccessTokenProvider();     
+//        OAuth2AccessToken accessToken = provider.obtainAccessToken(resource(), new DefaultAccessTokenRequest());
+//        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource(), oauth2ClientContext);
+//        restTemplate.setAccessTokenProvider((AccessTokenProvider) accessToken);
+//        return restTemplate;
+
+    	return new OAuth2RestTemplate(resource(), oauth2ClientContext);
+    }
+
+    @Bean
+    protected OAuth2ProtectedResourceDetails resource() {
+    	List scopes = new ArrayList<String>(2);
+        scopes.add("write");
+        scopes.add("read");
+        ClientCredentialsResourceDetails resource = new ClientCredentialsResourceDetails();
+        resource.setClientId(clientID);
+        resource.setClientSecret(clientSecret);
+        resource.setAccessTokenUri(accessTokenUri);
+        resource.setGrantType(grantType);
+        resource.setClientAuthenticationScheme(AuthenticationScheme.header);
+        resource.setTokenName("access_token");
+        resource.setAuthenticationScheme(AuthenticationScheme.header);
+        resource.setScope(scopes);
+        resource.setClientAuthenticationScheme(AuthenticationScheme.form);
+        return resource;
+    }
+
 }
--- pallette-commerce/src/main/java/com/pallette/config/WebSecurityConfig.java
+++ pallette-commerce/src/main/java/com/pallette/config/WebSecurityConfig.java
@@ -1,10 +1,6 @@
 package com.pallette.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,13 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.common.AuthenticationScheme;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 import com.pallette.web.security.CustomAuthenticationProvider;
 import com.pallette.web.security.CustomCredentialsService;
@@ -28,7 +17,6 @@ import com.pallette.web.security.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Client
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -39,18 +27,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private LogoutSuccessHandler logoutSuccessHandler;
-	
-	@Value("${security.oauth2.client.access-token-uri}")
-	private String accessTokenUri;
-
-    @Value("${security.oauth2.client.grant-type}")
-    private String grantType;
-
-    @Value("${security.oauth2.client.client-id}")
-    private String clientID;
-
-    @Value("${security.oauth2.client.client-secret}")
-    private String clientSecret;
 	
     @Bean
 	public PasswordEncoder passwordEncoder(){
@@ -88,28 +64,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.authenticationProvider(customAuthenticationProvider);
 		auth.userDetailsService(customCredentialsService);
 	}
-	
-	/**
-     * The heart of our interaction with the resource; handles redirection for authentication, access tokens, etc.
-     * @param oauth2ClientContext
-     * @return
-     */
-    @Bean
-    public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {
-        return new OAuth2RestTemplate(resource(), oauth2ClientContext);
-    }
-
-    private OAuth2ProtectedResourceDetails resource() {
-    	List<String> scope = new ArrayList<String>();
-    	ClientCredentialsResourceDetails resource = new ClientCredentialsResourceDetails();
-        resource.setClientId(clientID);
-        resource.setClientSecret(clientSecret);
-        resource.setAccessTokenUri(accessTokenUri);
-        resource.setGrantType(grantType);
-        resource.setScope(scope);
-        resource.setAuthenticationScheme(AuthenticationScheme.query);
-        resource.setTokenName("access_token");
-        return resource;
-    }
-
 }
