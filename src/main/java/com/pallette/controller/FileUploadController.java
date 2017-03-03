aec341ab24fc7a2a15b83cbdbace53b219caf90a
Roshan <roshantiwary@gmail.com>
2017-03-03 19:00:03 +0530

    File uploader logic

---
+++ src/main/java/com/pallette/controller/FileUploadController.java
@@ -0,0 +1,61 @@
+package com.pallette.controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/rest/api/v1")
+public class FileUploadController {
+
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    
+    @PostMapping("/products/upload")
+    public ResponseEntity<?> uploadProduct(
+            @RequestParam("file") MultipartFile uploadfile) {
+
+        logger.debug("Single file upload!");
+
+        if (uploadfile.isEmpty()) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(uploadfile));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded - " +
+                uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+
+    }
+    
+    //save file
+    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+
+        for (MultipartFile file : files) {
+
+            if (file.isEmpty()) {
+                continue; //next pls
+            }
+
+          //todo file processing
+        }
+
+    }
+}
