dda1ac15f6a625c9f66d98417cfe71ec5ec580a9
Roshan <roshantiwary@gmail.com>
2017-03-24 22:59:48 +0530

    Added ID Generator code for profile and order generation

--- src/main/java/com/pallette/web/security/CustomTokenEnhancer.java
+++ src/main/java/com/pallette/web/security/CustomTokenEnhancer.java
@@ -3,19 +3,27 @@ package com.pallette.web.security;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+import com.pallette.repository.SequenceDao;
+
 public class CustomTokenEnhancer implements TokenEnhancer{
 
+	private static final String HOSTING_SEQ_KEY = "hosting";
+	
+	@Autowired
+	private SequenceDao sequenceDao;
+	
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken,
 			OAuth2Authentication authentication) {
 		 Map<String, Object> additionalInfo = new HashMap<>();
-	        additionalInfo.put("orderId", "ORDER001");
-	        additionalInfo.put("profileId", "PROFILE0001");
+	        additionalInfo.put("orderId", sequenceDao.getNextSequenceId(HOSTING_SEQ_KEY));
+	        additionalInfo.put("profileId", sequenceDao.getNextSequenceId(HOSTING_SEQ_KEY));
 	        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
 	        return accessToken;
 	}
