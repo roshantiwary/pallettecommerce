f750ac46fcad48ba1d3db79b42810b0c6fb03651
Roshan <roshantiwary@gmail.com>
2017-04-03 16:44:25 +0530

    Introduced new payment property file

--- src/main/java/com/pallette/payment/payu/PaymentIntegrator.java
+++ src/main/java/com/pallette/payment/payu/PaymentIntegrator.java
@@ -23,6 +23,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -41,11 +45,18 @@ import com.pallette.repository.ProductRepository;
  *
  * @author Pallette
  */
+@Configuration
 @Component
+@PropertySource("classpath:payment.properties")
 public class PaymentIntegrator {
 	
 	private static final Logger log = LoggerFactory.getLogger(PaymentIntegrator.class);
 
+	@Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+	
 	@Value("${payment.salt}")
 	private String salt;
 	
