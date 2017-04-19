971face97ba154df070edfecbb1500c8526754ce
Vivek Dwivedi <dwivedivivek13@gmail.com>
2017-04-19 16:39:29 +0530

    Circular Dependecny Issue Fixes

--- src/main/java/com/pallette/service/UserService.java
+++ src/main/java/com/pallette/service/UserService.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.pallette.beans.AccountBean;
@@ -27,9 +26,6 @@ public class UserService {
 
 	@Autowired
 	private AccountService accountService;
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
 	
 	@Autowired
 	RoleRepository roleRepository;
