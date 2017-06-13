41239f3ea42ddeadb5c4af38d4825d59d2ca1b63
Abhishek Mallick <amall3@sapient.com>
2017-06-13 16:13:17 +0530

    Removing hardcoded values and getting from Repository

--- src/main/java/com/pallette/solr/controller/SolrController.java
+++ src/main/java/com/pallette/solr/controller/SolrController.java
@@ -8,12 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.query.result.SolrResultPage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pallette.browse.documents.ProductDocument;
+import com.pallette.browse.repository.ProductRepository;
 import com.pallette.solr.domain.SolrProduct;
 import com.pallette.solr.repository.SolrProductRepository;
 import com.pallette.solr.response.ProductsResponse;
@@ -21,53 +24,54 @@ import com.pallette.solr.response.ProductsResponse;
 @RestController
 @RequestMapping("/rest/api/v1")
 public class SolrController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(SolrController.class);
-	
-	@Autowired
-	SolrProductRepository productRepository;
 
-	@PostMapping("/index/products")
-    public ResponseEntity<?> indexProducts() {
-        logger.debug("Product file upload");
-       SolrProduct product = new SolrProduct();
-       product.setId("123");
-       product.setProductDescription("zindagi");
-       product.setProductSlug("adf");
-       product.setProductStatus("productStatus");
-       product.setProductTitle("productTitle");
-       productRepository.save(product);
-       
-       
-       SolrProduct product1 = new SolrProduct();
-       product.setId("456");
-       product.setProductDescription("roger");
-       product.setProductSlug("copy");
-       product.setProductStatus("true");
-       product.setProductTitle("tiger");
-       productRepository.save(product1);
-       
-       SolrProduct product2 = new SolrProduct();
-       product.setId("789");
-       product.setProductDescription("shah");
-       product.setProductSlug("salman");
-       product.setProductStatus("rukh");
-       product.setProductTitle("khan");
-       productRepository.save(product2);
-       
-       return new ResponseEntity("Successfully uploaded" , new HttpHeaders(), HttpStatus.OK);
-    }
+	private static final Logger logger = LoggerFactory.getLogger(SolrController.class);
+
+	@Autowired
+	SolrProductRepository solrProductRepository;
 	
-	@GetMapping("/index/products")
-    public ProductsResponse getProducts() {
-        logger.debug("Product file upload");
-      
-        SolrResultPage<SolrProduct> products = (SolrResultPage<SolrProduct>) productRepository.findAll();
-       
-       ProductsResponse response = new ProductsResponse();
-       response.setProducts(products);
-       
-       return response;
-    }
+	/**
+	 * The Product repository.
+	 */
+	@Autowired
+	private ProductRepository productRepository;
+
+	/**
+	 * 
+	 * @return
+	 */
+	@PostMapping("/index/solr/products")
+	public ResponseEntity<?> indexProducts() {
+
+		logger.debug("Inside SolrController.indexProducts()");
+		List<ProductDocument> products = productRepository.findAll();
+		if (null != products && !products.isEmpty()) {
+			for (ProductDocument product : products) {
+
+				SolrProduct solrProduct = new SolrProduct();
+				solrProduct.setId(product.getId());
+				solrProduct.setProductDescription(product.getProductDescription());
+				solrProduct.setProductSlug(product.getProductSlug());
+				solrProduct.setProductStatus(product.getProductStatus());
+				solrProduct.setProductTitle(product.getProductTitle());
+				solrProductRepository.save(solrProduct);
+			}
+		}
+		return new ResponseEntity("Successfully uploaded", new HttpHeaders(), HttpStatus.OK);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/get/solr/products", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProductsResponse getProductsFromSolr() {
+		
+		logger.debug("Inside SolrController.getProducts()");
+		SolrResultPage<SolrProduct> products = (SolrResultPage<SolrProduct>) solrProductRepository.findAll();
+		ProductsResponse response = new ProductsResponse();
+		response.setProducts(products);
+		return response;
+	}
 
 }
