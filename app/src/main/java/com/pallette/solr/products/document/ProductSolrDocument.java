909493df64343eb7d53af9aaa9fa5b1983d6638e
Roshan Kumar <roshantiwary@gmail.com>
2020-04-07 16:59:50 -0400

    Added solr indexing code

--- app/src/main/java/com/pallette/solr/products/document/ProductSolrDocument.java
+++ app/src/main/java/com/pallette/solr/products/document/ProductSolrDocument.java
@@ -1,81 +1,151 @@
 package com.pallette.solr.products.document;
-import org.apache.solr.client.solrj.beans.Field;
+
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
-import org.springframework.data.solr.core.mapping.Dynamic;
-import org.springframework.data.solr.core.mapping.Indexed;
 import org.springframework.data.solr.core.mapping.SolrDocument;
+import org.apache.solr.client.solrj.beans.Field;
 
-import java.util.Collection;
-import java.util.Map;
-
-/**
- * Created by Roshan.
- */
-@SolrDocument(solrCoreName = "products")
+@SolrDocument(solrCoreName="products")
 public class ProductSolrDocument {
 
-    @Id
-    @Indexed
-    @Field("id")
-    private Long id;
+	@Id
+	@Field
+	private String id;
 
-    @Indexed
-    @Field("name")
-    private String name;
+	private Date timeCreated;
 
-    @Indexed
-    @Field("price")
-    private Long price;
+	@Field(value = "product_title")
+	private String productTitle;
 
-    @Indexed
-    @Field("description")
-    private Collection<String> description;
+	@Field(value = "product_slug")
+	private String productSlug;
 
-    @Dynamic
-    @Field("prop_*")
-    private Map<String, Long> dynamicProps;
+	@Field(value = "product_description")
+	private String productDescription;
 
-    public ProductSolrDocument() {
+	@Field(value = "product_status")
+	private String productStatus;
 
-    }
+	@Field(child=true, value = "product_category")
+	private String categoryDocument;
+	
+	public String getCategoryDocument() {
+		return categoryDocument;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setCategoryDocument(String categoryDocument) {
+		this.categoryDocument = categoryDocument;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getBrandDocument() {
+		return brandDocument;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setBrandDocument(String brandDocument) {
+		this.brandDocument = brandDocument;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public List<String> getSkuDocument() {
+		return skuDocument;
+	}
 
-    public Long getPrice() {
-        return price;
-    }
+	public void setSkuDocument(List<String> skuDocument) {
+		this.skuDocument = skuDocument;
+	}
 
-    public void setPrice(Long price) {
-        this.price = price;
-    }
 
-    public Collection<String> getDescription() {
-        return description;
-    }
+	@Field(child=true, value = "product_brand")
+	private String brandDocument;
+	
+	@Field(child=true, value = "product_sku")
+	private List<String> skuDocument;
 
-    public void setDescription(Collection<String> description) {
-        this.description = description;
-    }
+	public ProductSolrDocument() {
+		super();
+	}
 
-    public Map<String, Long> getDynamicProps() {
-        return dynamicProps;
-    }
+	/**
+	 * @return the productTitle
+	 */
+	public String getProductTitle() {
+		return productTitle;
+	}
 
-    public void setDynamicProps(Map<String, Long> dynamicProps) {
-        this.dynamicProps = dynamicProps;
-    }
+	/**
+	 * @param productTitle
+	 *            the productTitle to set
+	 */
+	public void setProductTitle(String productTitle) {
+		this.productTitle = productTitle;
+	}
+
+	/**
+	 * @return the productSlug
+	 */
+	public String getProductSlug() {
+		return productSlug;
+	}
+
+	/**
+	 * @param productSlug
+	 *            the productSlug to set
+	 */
+	public void setProductSlug(String productSlug) {
+		this.productSlug = productSlug;
+	}
+
+	/**
+	 * @return the productDescription
+	 */
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	/**
+	 * @param productDescription
+	 *            the productDescription to set
+	 */
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	/**
+	 * @return the productStatus
+	 */
+	public String getProductStatus() {
+		return productStatus;
+	}
+
+	/**
+	 * @param productStatus
+	 *            the productStatus to set
+	 */
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getTimeCreated() {
+		return timeCreated;
+	}
+
+	public void setTimeCreated(Date timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
